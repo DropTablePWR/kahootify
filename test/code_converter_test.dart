61c75311146495ea8aec7d041471c89f213da927
a-mirecki <a.mirecki99@gmail.com>
2021-05-11 21:12:37 +0200

    fix

--- test/code_converter_test.dart
+++ test/code_converter_test.dart
@@ -1,5 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:kahootify_server/utils/code_converter.dart';
-import 'package:test/test.dart';
+
 
 main() {
   test('Ip should be correctly encoded and decoded', () {
