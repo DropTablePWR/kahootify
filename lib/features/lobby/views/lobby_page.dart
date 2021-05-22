8b8a5bcbc92d38c993cf376df415fc3f8aa43f01
WojciechKaluzinski <56256988+WojciechKaluzinski@users.noreply.github.com>
2021-05-22 23:25:13 +0200

    KAH-54 lobby screen design fixes

--- lib/features/lobby/views/lobby_page.dart
+++ lib/features/lobby/views/lobby_page.dart
@@ -3,11 +3,13 @@ import 'dart:convert';
 
 import 'package:backdrop/backdrop.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:kahootify/color_consts.dart';
-import 'package:kahootify/widgets/player_number_indicator.dart';
+import 'package:kahootify/features/lobby/views/players_list_view.dart';
 import 'package:kahootify_server/models/player_info.dart';
 import 'package:kahootify_server/models/server_info.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+
+import 'widgets.dart';
 
 class LobbyPage extends StatefulWidget {
   final bool isHost;
@@ -45,174 +47,84 @@ class _LobbyPageState extends State<LobbyPage> {
           break;
       }
     });
-
     super.initState();
   }
 
+  Widget getFAB() {
+    if (widget.isHost) {
+      if (allReady) {
+        return StartGameButton();
+      }
+      return SizedBox.shrink();
+    } else {
+      return IAmReadyButton(
+        iAmReady: iAmReady,
+        onPressed: () {
+          setState(() {
+            iAmReady = !iAmReady;
+            print(iAmReady.toString());
+            widget.input.add(jsonEncode(playerInfo.copyWith(ready: iAmReady).toJson()));
+          });
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BackdropScaffold(
-      backLayerBackgroundColor: kBackgroundGreenColor,
-      backgroundColor: kBackgroundLightColor,
-      appBar: BackdropAppBar(
-        title: Text("LOBBY"),
-        backgroundColor: kBackgroundGreenColor,
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 20.0),
-            child: IconButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Container(
-                            height: 300,
-                            width: 300,
-                            child: QrImage(
-                              data: widget.initialServerInfo.qrCode,
-                              version: QrVersions.auto,
-                              size: 300.0,
-                            ),
-                          ),
-                          Text(widget.initialServerInfo.code)
-                        ],
-                      ),
-                    );
-                  },
-                );
-              },
-              icon: Icon(Icons.qr_code),
+    return OrientationBuilder(builder: (context, orientation) {
+      if (orientation == Orientation.portrait) {
+        return BackdropScaffold(
+          headerHeight: MediaQuery.of(context).size.height * 0.5,
+          stickyFrontLayer: true,
+          backLayerBackgroundColor: kBackgroundGreenColor,
+          backgroundColor: kBackgroundLightColor,
+          appBar: BackdropAppBar(
+            title: Text("LOBBY"),
+            backgroundColor: kBackgroundGreenColor,
+            actions: [
+              DisplayQRCodeButton(qrCode: widget.initialServerInfo.qrCode, code: widget.initialServerInfo.code),
+            ],
+          ),
+          backLayer: BackLayer(serverInfo: widget.initialServerInfo, isPortrait: true),
+          frontLayer: Scaffold(
+            backgroundColor: kBackgroundLightColor,
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: getFAB(),
+            body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: PlayersListView(playersList: playersList, maxNumberOfPlayers: widget.initialServerInfo.maxNumberOfPlayers, isHorizontal: false),
             ),
           ),
-        ],
-      ),
-      backLayer: _BackLayer(serverInfo: widget.initialServerInfo),
-      frontLayer: Scaffold(
-        backgroundColor: kBackgroundLightColor,
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: !widget.isHost
-            ? FloatingActionButton.extended(
-                backgroundColor: iAmReady ? kBackgroundGreenColor : kBasedBlackColor,
-                label: Text(iAmReady ? "I am ready" : "I am not ready"),
-                onPressed: () {
-                  setState(() {
-                    iAmReady = !iAmReady;
-                    print(iAmReady.toString());
-                    widget.input.add(jsonEncode(playerInfo.copyWith(ready: iAmReady).toJson()));
-                  });
-                },
-                icon: Icon(iAmReady ? Icons.check : Icons.clear, color: Colors.white, size: 50),
-                shape: OutlineInputBorder(),
-              )
-            : allReady
-                ? FloatingActionButton.extended(
-                    backgroundColor: kBackgroundGreenColor,
-                    label: Text("Start a game"),
-                    onPressed: () {
-                      setState(() {
-                        iAmReady = !iAmReady;
-                        print("Start a game");
-                        //TODO Rozpoczęcie rozgrywki
-                      });
-                    },
-                    icon: Icon(Icons.outlined_flag, color: Colors.white, size: 50),
-                    shape: OutlineInputBorder(),
-                  )
-                : SizedBox.shrink(),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          child: ListView.builder(
-            itemCount: playersList.length,
-            padding: EdgeInsets.only(top: 35),
-            itemBuilder: (BuildContext context, int index) => _PlayersListItem(playerInfo: playersList[index]),
+        );
+      } else {
+        return Scaffold(
+          backgroundColor: kBackgroundLightColor,
+          appBar: AppBar(
+            title: Text("LOBBY"),
+            backgroundColor: kBackgroundGreenColor,
+            actions: [
+              DisplayQRCodeButton(qrCode: widget.initialServerInfo.qrCode, code: widget.initialServerInfo.code),
+            ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class _PlayersListItem extends StatelessWidget {
-  final PlayerInfo playerInfo;
-
-  const _PlayersListItem({required this.playerInfo});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      color: playerInfo.ready ? kBackgroundGreenColor : kBasedBlackColor,
-      elevation: 10,
-      child: SizedBox(
-        height: 50.0,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(child: Text(playerInfo.name, style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold))),
-            Container(child: Icon(playerInfo.ready ? Icons.check : Icons.clear, color: Colors.white, size: 50)),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _BackLayer extends StatelessWidget {
-  final ServerInfo serverInfo;
-
-  const _BackLayer({required this.serverInfo});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30),
-        child: Column(children: [
-          SizedBox(height: 30),
-          _ServerInfoWidget(iconData: Icons.videogame_asset, text: serverInfo.name),
-          SizedBox(height: 30),
-          _ServerInfoWidget(iconData: Icons.category, text: serverInfo.category.name),
-          SizedBox(height: 30),
-          _ServerInfoWidget(iconData: Icons.format_list_numbered_outlined, text: serverInfo.numberOfQuestions.toString()),
-          SizedBox(height: 30),
-          _ServerInfoWidget(iconData: Icons.timelapse, text: serverInfo.answerTimeLimit.toString() + ' s'),
-          SizedBox(height: 30),
-          _ServerInfoWidget(iconData: Icons.people, customChild: PlayerNumberIndicator(serverInfo: serverInfo))
-        ]),
-      ),
-    );
-  }
-}
-
-class _ServerInfoWidget extends StatelessWidget {
-  final IconData iconData;
-  final Widget? customChild;
-  final String? text;
-
-  const _ServerInfoWidget({Key? key, required this.iconData, this.customChild, this.text}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Icon(iconData, size: 60),
-        SizedBox(width: 30),
-        customChild ??
-            Flexible(
-              child: Text(
-                text ?? "",
-                style: TextStyle(fontSize: 35, color: kBasedBlackColor, fontWeight: FontWeight.bold),
-              ),
+          floatingActionButton: getFAB(),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: PlayersListView(playersList: playersList, maxNumberOfPlayers: widget.initialServerInfo.maxNumberOfPlayers, isHorizontal: true),
+                ),
+                Expanded(
+                    child: SingleChildScrollView(
+                        padding: EdgeInsets.only(bottom: 100),
+                        child: SizedBox(width: 200, child: BackLayer(serverInfo: widget.initialServerInfo, isPortrait: false))))
+              ],
             ),
-      ],
-    );
+          ),
+        );
+      }
+    });
   }
 }
---
+++ lib/features/lobby/views/widgets.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:kahootify/color_consts.dart';
+import 'package:kahootify_server/models/server_info.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class BackLayer extends StatelessWidget {
+  final ServerInfo serverInfo;
+  final bool isPortrait;
+
+  const BackLayer({required this.serverInfo, required this.isPortrait});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Column(children: [
+          SizedBox(height: 20),
+          ServerInfoWidget(name: "SERVER NAME: ", text: serverInfo.name, isPortrait: isPortrait),
+          SizedBox(height: 20),
+          ServerInfoWidget(name: "CATEGORY: ", text: serverInfo.category.name, isPortrait: isPortrait),
+          SizedBox(height: 20),
+          ServerInfoWidget(name: "NUMBER OF QUESTIONS: ", text: serverInfo.numberOfQuestions.toString(), isPortrait: isPortrait),
+          SizedBox(height: 20),
+          ServerInfoWidget(name: "TIME TO ANSWER: ", text: serverInfo.answerTimeLimit.toString() + ' s', isPortrait: isPortrait)
+        ]),
+      ),
+    );
+  }
+}
+
+class ServerInfoWidget extends StatelessWidget {
+  final String name;
+  final String? text;
+  final bool isPortrait;
+
+  const ServerInfoWidget({Key? key, required this.name, this.text, this.isPortrait = false}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Text(
+            name,
+            style: TextStyle(fontSize: 15, color: isPortrait ? Colors.white : kBasedBlackColor, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(width: 25),
+        Expanded(
+          child: Text(
+            text ?? "",
+            style: TextStyle(fontSize: 15, color: isPortrait ? Colors.white : kBasedBlackColor, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class IAmReadyButton extends StatelessWidget {
+  final bool iAmReady;
+  final Function() onPressed;
+
+  const IAmReadyButton({Key? key, required this.iAmReady, required this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicButton(
+      style: NeumorphicStyle(
+        color: iAmReady ? kBackgroundGreenColor : kBasedBlackColor,
+        depth: iAmReady ? -10 : 10,
+        oppositeShadowLightSource: false,
+        shadowDarkColor: kBasedBlackColor,
+        shadowDarkColorEmboss: kBasedBlackColor,
+        shadowLightColor: kBackgroundLightColor,
+        shadowLightColorEmboss: kBackgroundLightColor,
+        shape: NeumorphicShape.concave,
+        boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(8)),
+      ),
+      child: Container(
+        width: 140,
+        height: 30,
+        child: iAmReady
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  NeumorphicIcon(Icons.check, size: 30),
+                  Text("I am ready", style: TextStyle(color: Colors.white)),
+                ],
+              )
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  NeumorphicIcon(Icons.clear, size: 30),
+                  Text("I am not ready", style: TextStyle(color: Colors.white)),
+                ],
+              ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
+
+class StartGameButton extends StatelessWidget {
+  const StartGameButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      backgroundColor: kBackgroundGreenColor,
+      label: Text("Start a game"),
+      onPressed: () {
+        print("Start a game");
+        //TODO Rozpoczęcie rozgrywki
+      },
+      icon: Icon(Icons.outlined_flag, color: Colors.white, size: 50),
+      shape: OutlineInputBorder(),
+    );
+  }
+}
+
+class DisplayQRCodeButton extends StatelessWidget {
+  final String qrCode;
+  final String code;
+
+  const DisplayQRCodeButton({
+    Key? key,
+    required this.qrCode,
+    required this.code,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 20.0),
+      child: IconButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      height: 200,
+                      width: 200,
+                      child: QrImage(
+                        data: qrCode,
+                        version: QrVersions.auto,
+                        size: 300.0,
+                      ),
+                    ),
+                    Text(code)
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        icon: Icon(Icons.qr_code),
+      ),
+    );
+  }
+}
